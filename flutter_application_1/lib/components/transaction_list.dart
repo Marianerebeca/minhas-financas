c837f0a0f3c1daf122838d18de642c03cffca804
Mariane <marianelobo16@hotmail.com>
2023-04-06 23:15:05 -0300

    feat: minhas financas

---
+++ flutter_application_1/lib/components/transaction_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/transaction_item.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+  const TransactionList(this.transactions, this.onRemove, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                SizedBox(height: 20),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/financas.png',
+                    width: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return TransactionItem(tr: tr, onRemove: onRemove);
+            },
+          );
+  }
+}
+
+
+
---
+++ flutter_application_1/lib/models/transaction.dart
@@ -0,0 +1,9 @@
+class Transaction{
+   late String id;
+   late String title;
+   late double value;
+   late DateTime date;
+
+  Transaction({required this.id, required this.title, required this.value, required this.date});
+
+}
